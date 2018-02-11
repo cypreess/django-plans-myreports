e6ce78e6d6b45201705cefeddb3b366976e8fd02
Kris Dorosz <cypreess@gmail.com>
2018-02-11 13:39:09 +0100

    Add CSV report

--- plans_reports/urls.py
+++ plans_reports/urls.py
@@ -7,6 +7,7 @@ from plans_reports.views import InvoiceView
 urlpatterns = patterns('',
     url(r'^noninvoiced/(?P<pk>\d+)/$', staff_member_required(DetailView.as_view(model=NonInvoicedReport)), name='plans_raport_noninvoiced'),
     url(r'^invoiced/(?P<pk>\d+)/$', staff_member_required(DetailView.as_view(model=InvoicedReport)), name='plans_raport_invoiced'),
+    url(r'^invoiced/(?P<pk>\d+)/csv/$', staff_member_required(DetailView.as_view(model=InvoicedReport, template_name='plans_reports/invoicedreports_detail.csv')), name='plans_raport_invoiced_csv'),
     url(r'^invoices/(?P<year>\d+)/(?P<month>\d+)/$', staff_member_required(InvoiceView.as_view()), name='plans_raport_invoices'),
 
 
